89e954c51d57c38522336367eda7ad1683a89b91
Golden-I <65423579+Golden-I@users.noreply.github.com>
2023-02-24 22:54:06 +0200

    "Part V - Authentication"

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const { stringify } = require("querystring");
+const { nextTick } = require("process");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+userSchema.pre("save", async (next) => {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
